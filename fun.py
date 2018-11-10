f82a8783b03b81626acecae0adf374d3c56c04af
speselized <43604908+speselized@users.noreply.github.com>
2018-11-10 15:47:02 +1030

    Update fun.py

--- fun.py
+++ fun.py
@@ -41,27 +41,6 @@ class Fun:
         await self.client.say(random.choice(["It is certain"," It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes"," Most likely","Outlook good","Yes","Signs point to yes"," Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]))
         await self.client.add_reaction("🎱")
 
-async def on_message(message):
-
-    elif "goodnight" in text or "good night" in text \
-            or "going to sleep" in text:
-        await client.add_reaction(message, "👋")
-
-    elif "good morning" in text or "hello everyone" in text or \
-            "good evening" in text or "sup fuckers" in text:
-        if random.randint(0,1) == 0:
-            await client.add_reaction(message, wavefenriz)
-        else:
-            await client.add_reaction(message, wavedog)
-
-    elif "fuck you" in text:
-        await client.add_reaction(message, salt)
-
-    elif "ah!" in text:
-        await client.add_reaction(message, ah)
-
-    elif "reeeee" in text:
-        await client.add_reaction(message, bornagain)
 
 
     @commands.command(pass_context=True)
