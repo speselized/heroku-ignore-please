efcb5cbdcb42eea23761d902fdc0c675d853ddb9
speselized <43604908+speselized@users.noreply.github.com>
2018-11-15 18:41:23 +1030

    Update moderations.py

--- moderations.py
+++ moderations.py
@@ -27,29 +27,17 @@ time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
 time_dict = {"h":3600, "s":1, "m":60, "d":86400}
 
 class TimeConverter(commands.Converter):
-
     async def convert(self):
-
         args = self.argument.lower()
-
         matches = re.findall(time_regex, args)
-
         time = 0
-
         for v, k in matches:
-
             try:
-
                 time += time_dict[k]*float(v)
-
             except KeyError:
-
                 raise commands.BadArgument("{} is an invalid time-key! h/m/s/d are valid!".format(k))
-
             except ValueError:
-
                 raise commands.BadArgument("{} is not a number!".format(v))
-
         return time
 
 class Moderations:
