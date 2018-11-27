c67e2bf239eb09afbfe6b34ba062a20f855c80c0
speselized <43604908+speselized@users.noreply.github.com>
2018-11-27 18:26:49 +1030

    Update fun.py

--- fun.py
+++ fun.py
@@ -44,9 +44,8 @@ class Fun:
     @commands.command(name="8ball")
     async def _ball(self, ctx):
         """Magic 8ball. SPOOKY"""
-        message = await self.client.say(random.choice(["It is certain"," It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes"," Most likely","Outlook good","Yes","Signs point to yes"," Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]))
-        emote = get(client.get_all_emojis(),name='8ball')
-        await self.client.add_reaction(message, emote)
+        await self.client.say(random.choice(["It is certain"," It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes"," Most likely","Outlook good","Yes","Signs point to yes"," Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]))
+        await self.client.add_reaction("🎱")
 
 
 
@@ -163,12 +162,6 @@ class Fun:
 
 
 
-
-    @commands.command(pass_context=True)
-    async def lapdance(self, ctx, member : discord.Member):
-        """OOoooooooOooOooOHH"""
-            await self.client.delete_message(ctx.message)
-            await self.client.say('***Gives {} a sexy beautiful hot sweating pulsing lesbian gay porn lapdance!***'.format(member))
 
     @commands.command(pass_context=True)
     async def ping(self, ctx):
