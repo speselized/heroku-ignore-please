cac6d3f3f90c643dfd801d04ace1057560ba9938
Jake <43604908+speselized@users.noreply.github.com>
2018-12-27 23:08:05 +1030

    Update fun.py

--- fun.py
+++ fun.py
@@ -45,7 +45,6 @@ class Fun:
     async def _ball(self, ctx):
         """Magic 8ball. SPOOKY"""
         await self.client.say(random.choice(["It is certain"," It is decidedly so","Without a doubt","Yes, definitely","You may rely on it","As I see it, yes"," Most likely","Outlook good","Yes","Signs point to yes"," Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]))
-        await self.client.add_reaction("🎱")
 
 
 
@@ -57,7 +56,7 @@ class Fun:
             await self.client.say(ctx.message.author.mention + ", you cannot slap yourself.")
             return
         else:
-            await self.client.say("**{0} slaps {1} around a bit with a large, girthy trout** <:fishslap:504195777564770314> :hand_splayed: :dizzy_face: ".format(author, member))
+            await self.client.say("**{0} slaps {1} around a bit with a large, girthy trout** :hand_splayed: :dizzy_face: ".format(author, member))
 
 
 
